a144f9e1c071f55d79a7d59d963950a816b1a436
Sbrubles08 <92830961+Sbrubles08@users.noreply.github.com>
2022-12-05 20:43:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyDlFY3juD2snwlOnhnG7MGJOzYdKTbKahk",
+  authDomain: "projeto-c67.firebaseapp.com",
+  databaseURL: "https://projeto-c67-default-rtdb.firebaseio.com",
+  projectId: "projeto-c67",
+  storageBucket: "projeto-c67.appspot.com",
+  messagingSenderId: "438447682785",
+  appId: "1:438447682785:web:760a1734896c5422fd33a0"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
